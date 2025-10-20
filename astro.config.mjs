c1d5994b6c47c59e2904ec6301f5112b805b7149
Victor Hugo Amador <v.sk8er.b0i@hotmail.com>
2025-10-20 03:55:48 -0600

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://IEVN1001-22001439.github.io/viktor-portfolio/',
+  base: '/viktor-portfolio/',
 });
